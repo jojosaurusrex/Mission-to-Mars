c53be13d986e4bfc9b97c809e82d8b305e0312ab
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-04-23 17:34:11 -0700

    End of 10.5.1

---
+++ scraping.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+# In[2]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[3]:
+
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+# elements with a specific combination of tag (div) and attribute 
+#(list_text). Also telling it to wait
+
+
+# In[4]:
+
+
+html = browser.html #using splinter initially
+news_soup = soup(html, 'html.parser') #using beautifulSoup for the rest
+slide_elem = news_soup.select_one('div.list_text') 
+#does this only allow to select a singular value? if so, it makes sense
+
+# CSS works from right to left, so when using select_one, the first 
+#matching element is the actually the last one?
+
+#word for word from module
+# the first matching element returned will be a <li /> element with a 
+#class of slide and all nested elements within it.
+
+
+# In[5]:
+
+
+slide_elem.find('div', class_='content_title')
+
+
+# In[6]:
+
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+
+# In[7]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+
+# ### Featured Images
+
+# In[8]:
+
+
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+
+# In[9]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[10]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[11]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[12]:
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+
+# In[14]:
+
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['description', 'Mars', 'Earth']
+df.set_index('description', inplace=True)
+df
+
+
+# In[16]:
+
+
+df.to_html()
+
+
+# In[17]:
+
+
+browser.quit()
+
+
+# In[ ]:
+
+
+
+

fc9f7201369373c300e901a832be6dd980e8886e
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-04-24 16:23:26 -0700

    Done with 10.5.3

--- scraping.py
+++ scraping.py
@@ -7,129 +7,161 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 # In[2]:
 
+def scrape_all():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 
 # In[3]:
 
+def mars_news(browser):
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Visit the mars nasa news site / Scrape Mars News
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-# elements with a specific combination of tag (div) and attribute 
-#(list_text). Also telling it to wait
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
+    # elements with a specific combination of tag (div) and attribute 
+    #(list_text). Also telling it to wait
 
-# In[4]:
+    # In[4]:
 
+    html = browser.html #using splinter initially
+    news_soup = soup(html, 'html.parser') #using beautifulSoup for the rest
+   
+    try:
+        slide_elem = news_soup.select_one('div.list_text') 
+        #does this only allow to select a singular value? if so, it makes sense
 
-html = browser.html #using splinter initially
-news_soup = soup(html, 'html.parser') #using beautifulSoup for the rest
-slide_elem = news_soup.select_one('div.list_text') 
-#does this only allow to select a singular value? if so, it makes sense
+        # CSS works from right to left, so when using select_one, the first 
+        #matching element is the actually the last one?
 
-# CSS works from right to left, so when using select_one, the first 
-#matching element is the actually the last one?
+        #word for word from module
+        # the first matching element returned will be a <li /> element with a 
+        #class of slide and all nested elements within it.
 
-#word for word from module
-# the first matching element returned will be a <li /> element with a 
-#class of slide and all nested elements within it.
+        # In[5]:
 
+        # slide_elem.find('div', class_='content_title')
 
-# In[5]:
+        # In[6]:
 
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # news_title
 
-slide_elem.find('div', class_='content_title')
+        # In[7]:
 
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+        # news_p
 
-# In[6]:
+    except AttributeError:
+        return None, None
 
-
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-
-# In[7]:
-
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    return news_title, news_p
 
 
 # ### Featured Images
 
 # In[8]:
 
+def featured_image(browser):
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
-
-
-# In[9]:
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # In[9]:
 
 
-# In[10]:
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # In[10]:
 
 
-# In[11]:
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # In[11]:
+
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # In[12]:
 
 
-# In[12]:
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    return img_url 
 
 
 # In[14]:
 
+def mars_facts():
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+    try:
+        # use 'read_html" to scrape the facts table into a dataframe
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
+    except BaseException:
+      return None
+
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+
+    return df.to_html(classes="table table-striped")
+
 
 
 # In[16]:
 
 
-df.to_html()
+# df.to_html()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
+
 
 
 # In[17]:
 
 
-browser.quit()
+# browser.quit()
 
 
 # In[ ]:
